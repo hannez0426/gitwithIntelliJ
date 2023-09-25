4a4d6b2889bc7f621b3c3777f1032ca01f105611
Zennah Asumbrado <zenleasumbrado@gmail.com>
2023-09-25 22:20:25 +0800

    prints hello world

--- src/activity01/Activity01.java
+++ src/activity01/Activity01.java
@@ -15,6 +15,7 @@ public class Activity01 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        System.out.print("Hello world!!!!!!");
     }
     
 }
